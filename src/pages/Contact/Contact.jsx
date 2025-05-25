b40406d50aee2a1cb2ce9de559f9be9577ed0788
Benedict Notoane <benni@MacBookPro.lan>
2025-05-25 12:48:07 +0200

    create contact page and styling

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import Button from "../../components/Button/Button";
+import styles from "./Contact.module.css";
 
 const Contact = () => {
   return (
-    <div>Contact</div>
-  )
-}
+    <div className={styles.contactWrapper}>
+      <form className={styles.contactForm} noValidate>
+        <fieldset>
+          <legend className={styles.formTitle}>Contact Us</legend>
+          <p className={styles.description}>
+            We would love to hear from you! If you have any questions or
+            feedback, feel free to reach out to us!
+          </p>
 
-export default Contact
+          <div className={styles.formGroup}>
+            <label htmlFor="name">Name:</label>
+            <input type="text" id="name" name="name" />
+          </div>
+          <div className={styles.formGroup}>
+            <label htmlFor="email">Email:</label>
+            <input type="email" id="email" name="email" />
+          </div>
+          <div className={styles.formGroup}>
+            <label htmlFor="message">Message:</label>
+            <textarea
+              id="message"
+              name="message"
+              className={styles.messageBox}
+              maxLength={300}
+              onResize={"none"}
+              placeholder="Write your message here. Max 500 characters"
+            ></textarea>
+          </div>
+          <Button type="submit" className={styles.submitButton}>Send</Button>
+        </fieldset>
+      </form>
+    </div>
+  );
+};
+
+export default Contact;
